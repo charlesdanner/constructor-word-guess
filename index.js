af6b69b500235e32f0cfd35091fb737c9ec6d52e
Charles Danner <danner.ced1@gmail.com>
2019-09-26 17:04:01 -0400

    mostly done with the game functionality. Game is on a loop though that won't break so i need to fix that.

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const Word = require('./word');
 const Letter = require('./letter')
 const prompt = require('prompt')
 
-
-
+let wordArr = []
 let word1 = new Word([
     new Letter("a"),
     new Letter("p"),
@@ -11,6 +10,7 @@ let word1 = new Word([
     new Letter("l"),
     new Letter("e")
 ]);
+wordArr.push(word1)
 
 let word2 = new Word([
     new Letter("m"),
@@ -21,6 +21,7 @@ let word2 = new Word([
     new Letter("y"),
     new Letter("s")
 ]);
+wordArr.push(word2)
 
 let word3 = new Word([
     new Letter("a"),
@@ -33,5 +34,82 @@ let word3 = new Word([
     new Letter("o"),
     new Letter("r")
 ]);
+wordArr.push(word3)
+
+let cli = () => {
+    let incorrectGuessesLeft = 9;
+    let currentWord = wordArr[(Math.floor((Math.random() * 3)))]
+    //console.log(currentWord)
+
+    let guessRightOrWrong = (userGuess) => {
+        var rightOrWrong = false;
+        for (var i = 0; i < currentWord.mysteryWord.length; i++) {                          //function checking to make sure the result matches one of the 
+            if (userGuess === currentWord.mysteryWord[i].value) {
+                rightOrWrong = true
+            }
+        } return rightOrWrong
+    }
+
+    let reset = () => {
+        incorrectGuessesLeft = 9;
+        for (var i = 0; i < currentWord.length; i++) {
+            currentWord[i].guessed = false;
+        }
+        currentWord = wordArr[(Math.floor((Math.random() * 3)))]
+        userGuesses()
+    }
+
+
+    prompt.start();
+
+    let userGuesses = () => {
+
+
+        if (incorrectGuessesLeft > 0 && currentWord.stringedWord().includes("_")) {    //if there are still guesses left and the word doesn't contain any underscores
+            prompt.get(['guess'], (err, result) => {
+                let userGuess = result.guess;
+                console.log(guessRightOrWrong(userGuess))                                    //get a prompt
+
+                if (guessRightOrWrong(userGuess)) {
+                    currentWord.guesses(userGuess)
+                    console.log(`
+Correct! You have ${incorrectGuessesLeft} incorrect guesses left! 
+${currentWord.stringedWord()}
+
+`);
+                    userGuesses()
+                    return;
+
+                } else if (!guessRightOrWrong(userGuess)) {
+                    incorrectGuessesLeft--
+                    if (incorrectGuessesLeft > 0) {
+                        console.log(
+                            `
+Incorrect! You have ${incorrectGuessesLeft} incorrect guesses left! 
+${currentWord.stringedWord()}
+
+`)
+                        userGuesses()
+                        return;
+                    } else if (incorrectGuessesLeft === 0) {
+                        console.log(`Oops! You lost this round, how about you try again.`)
+                        reset()
+                        return;
+                    }
+                }
+
+            })
+        } else if (incorrectGuessesLeft > 0 && !currentWord.stringedWord().includes("_")) {
+            console.log(`Congratulations! You correctly guessed ${currentWord.stringedWord().replace(/ /g, "")}`)
+            reset()
+            return;
+        }
+    }
+
+    userGuesses()
+
+}
+
+cli()
 
 
