74d0621336fbb6e6fa80631f1919c659eafce85e
Charles Danner <danner.ced1@gmail.com>
2019-09-27 10:26:24 -0400

    objects now have prototypes giving them methods instead of each individual one having them.

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-const Word = require('./word');
-const Letter = require('./letter')
-const inquirer = require('inquirer')
-
-let wordArr = []
-let userGuess = null
-let word1 = new Word([
-    new Letter("a"),
-    new Letter("p"),
-    new Letter("p"),
-    new Letter("l"),
-    new Letter("e")
-]);
-wordArr.push(word1)
-
-let word2 = new Word([
-    new Letter("m"),
-    new Letter("o"),
-    new Letter("n"),
-    new Letter("k"),
-    new Letter("e"),
-    new Letter("y"),
-    new Letter("s")
-]);
-wordArr.push(word2)
-
-let word3 = new Word([
-    new Letter("a"),
-    new Letter("l"),
-    new Letter("l"),
-    new Letter("i"),
-    new Letter("g"),
-    new Letter("a"),
-    new Letter("t"),
-    new Letter("o"),
-    new Letter("r")
-]);
-wordArr.push(word3)
-
 let cli = () => {
+    const Word = require('./word');
+    const Letter = require('./letter')              //requiring local as well as outside resources to make everything work.
+    const inquirer = require('inquirer')
+
+
+
+ 
+    let wordArr = []
+    let word1 = new Word([
+        new Letter("a"),
+        new Letter("p"),
+        new Letter("p"),
+        new Letter("l"),                    //words being constructed. Each letter is a Letter object
+        new Letter("e")
+    ]);
+    wordArr.push(word1)
+
+    let word2 = new Word([
+        new Letter("m"),
+        new Letter("o"),
+        new Letter("n"),
+        new Letter("k"),
+        new Letter("e"),
+        new Letter("y"),
+        new Letter("s")
+    ]);
+    wordArr.push(word2)
+
+    let word3 = new Word([
+        new Letter("a"),
+        new Letter("l"),
+        new Letter("l"),
+        new Letter("i"),
+        new Letter("g"),
+        new Letter("a"),
+        new Letter("t"),
+        new Letter("o"),
+        new Letter("r")
+    ]);
+    wordArr.push(word3)
+
+
+    
+    let userGuess = null                //global variables that get populated throughout the course of the application
     let incorrectGuessesLeft = 9;
-    let currentWord = wordArr[(Math.floor((Math.random() * 3)))]
+    let currentWord = wordArr[(Math.floor((Math.random() * 3)))]            //assigns the number of incorrect guesses users get and randomly chooses one of the words to be the objective.
     //console.log(currentWord)
 
     let guessRightOrWrong = (userGuess) => {
         var rightOrWrong = false;
-        for (var i = 0; i < currentWord.mysteryWord.length; i++) {                          //function checking to make sure the result matches one of the 
+        for (var i = 0; i < currentWord.mysteryWord.length; i++) {                          //function checking to make sure the result matches one of the letters in the word 
             if (userGuess === currentWord.mysteryWord[i].value) {
                 rightOrWrong = true
             }
@@ -52,69 +57,59 @@ let cli = () => {
     }
 
     let reset = () => {
-        incorrectGuessesLeft = 9;
-        for (var i = 0; i < currentWord.mysteryWord.length; i++) {
-            currentWord.mysteryWord[i].guessed = false;
+        for (var i = 0; i < currentWord.mysteryWord.length; i++) {              //function that resets the game by resetting all boolean values measuring whether the letter has been guessed or not to false
+            currentWord.mysteryWord[i].guessed = false;                         //makes the game play again
         }
-        currentWord = wordArr[(Math.floor((Math.random() * 3)))]
         cli()
     }
 
-
-
-    let logic = () => {
-
-        if (incorrectGuessesLeft > 0 && currentWord.stringedWord().includes("_")) {    //if there are still guesses left and the word doesn't contain any underscore
+    let logic = () => {         //the actual flow of the game goes in this function
+        if (incorrectGuessesLeft > 0 && currentWord.stringedWord().includes("_")) {    //if there are still guesses left and the word isn't completely guessed then prompt the user
             inquirer
-                .prompt([                                                       //get a prompt
+                .prompt([
                     {
                         name: "guess",
                         message: "Guess a letter that might be in the word"
                     }]).then(function (response) {
                         userGuess = response.guess;
-                        if (guessRightOrWrong(userGuess)) {
-                            currentWord.guesses(userGuess)
+                        if (guessRightOrWrong(userGuess)) {     //if the guess is a letter that is contained w/in the objective
+                            currentWord.guesses(userGuess)      //update the letters letter's guessed value and console log that they got the correct answer.
                             console.log(`
 Correct! You have ${incorrectGuessesLeft} incorrect guesses left! 
 ${currentWord.stringedWord()}
-        
+      
         `);
-        logic();
-        return;
-                        } else if (!guessRightOrWrong(userGuess)) {
+                            logic();                    //run the logic flow over again after a correct answer
+                            return;
+                        } else if (!guessRightOrWrong(userGuess)) {         //if the guessed letter is not w/in the objective word decrease the number of guesses they have left
                             incorrectGuessesLeft--
-                            if (incorrectGuessesLeft > 0) {
+                            if (incorrectGuessesLeft > 0) {             //consolelog that they were incorrect and rerun the logic flow
                                 console.log(
                                     `
 Incorrect! You have ${incorrectGuessesLeft} incorrect guesses left! 
 ${currentWord.stringedWord()}
         
-        `) 
-logic()
-return;
-                            } else if (incorrectGuessesLeft === 0) {
+        `)
+                                logic()
+                                return;
+                            } else if (incorrectGuessesLeft === 0) {        //after an incorrect answer is made if the guesses left total reaches zero the game tells the user they've lost and resets
                                 console.log(`Oops! You lost this round, how about you try again.`)
                                 reset()
                                 return;
                             }
                         } return;
-
                     })
             //console.log(guessRightOrWrong(userGuess))                                    
-        } else if (incorrectGuessesLeft > 0 && !currentWord.stringedWord().includes("_")) {
+        } else if (incorrectGuessesLeft > 0 && !currentWord.stringedWord().includes("_")) {         //if when the initial logic flow starts the user still has guesses left, but the word's letters have all been guessed, the user wins and the game resets.
             console.log(`Congratulations! You correctly guessed ${currentWord.stringedWord().replace(/ /g, "")}`)
             reset()
             return;
         }
         return;
-
     }
-
-console.log(currentWord.mysteryWord[0].guessed)
-     logic()
-      return;
+    logic()        //initial start up of logic flow.
+    return;
 }
-
-cli()
+cli()               //initial app start
 
 
--- word.js
+++ word.js
@@ -2,46 +2,22 @@ const Letter = require('./letter');         //getting the constructor function f
 
 let Word = function (mysteryWord) {
     this.mysteryWord = mysteryWord          //mysteryWord contains the array of corresponding Letter objects for each Word object
-    this.guesses = function (incomingArgument) {
-        for(var i = 0; i < mysteryWord.length; i++){
-            this.mysteryWord[i].checkedIfGuessed(incomingArgument)  //for each object in the mysteryWord array, it runs the checkedIfGuessed method in the Letter object and updates the checked boolean if necessary.
-        }
-    }
-    this.stringedWord = function () {
-        var word = [];
-        for (var i = 0; i < this.mysteryWord.length; i++) {
-            word.push(this.mysteryWord[i].returnValue()) //for each object in the mysteryWord array, the object is running the returnValue method and returning a character based on 
-                                                                                //whether the letter has been guessed yet or not
-        }
-        return word.join(" ")
+}
+
+Word.prototype.guesses = function (incomingArgument) {
+    for (var i = 0; i < this.mysteryWord.length; i++) {
+        this.mysteryWord[i].checkedIfGuessed(incomingArgument)  //for each object in the mysteryWord array, it runs the checkedIfGuessed method in the Letter object and updates the checked boolean if necessary.
     }
 }
 
-let apple = new Word([
-    new Letter("a"),
-    new Letter("p"),
-    new Letter("p"),
-    new Letter("l"),
-    new Letter("e")
-]);
-
-let monkeys = new Word([
-    new Letter("m"),
-    new Letter("o"),
-    new Letter("n"),
-    new Letter("k"),
-    new Letter("e"),
-    new Letter("y"),
-    new Letter("s")
-]);
-
-var guess = process.argv[2]
-
-var test = (X) =>{
-    apple.guesses(X) 
-    apple.stringedWord()
-}    
-test(guess)  
+Word.prototype.stringedWord = function () {
+    var word = [];
+    for (var i = 0; i < this.mysteryWord.length; i++) {
+        word.push(this.mysteryWord[i].returnValue()) //for each object in the mysteryWord array, the object is running the returnValue method and returning a character based on 
+        //whether the letter has been guessed yet or not
+    }
+    return word.join(" ")
+}
 
 module.exports = Word;
 
